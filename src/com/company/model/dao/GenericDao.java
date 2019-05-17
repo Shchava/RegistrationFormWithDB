2d09409dfa931ab2b147d7dfb12f9ce090ebf47f
yaroslav <yarosshc@gmail.com>
2019-05-17 12:40:52 +0300

    change interface to allow other types of primary keys

--- src/com/company/model/dao/GenericDao.java
+++ src/com/company/model/dao/GenericDao.java
@@ -2,9 +2,9 @@ package com.company.model.dao;
 
 import java.util.List;
 
-public interface GenericDao<T> extends  AutoCloseable{
+public interface GenericDao<T,Key> extends  AutoCloseable{
     void create (T entity);
-    T findById(int id);
+    T findByKey(Key key);
     List<T> findAll();
     void update(T entity);
     void delete(int id);
--- src/com/company/model/dao/NoteBookDao.java
+++ src/com/company/model/dao/NoteBookDao.java
@@ -2,5 +2,5 @@ package com.company.model.dao;
 
 import com.company.model.entity.NoteBook;
 
-public interface NoteBookDao extends GenericDao <NoteBook> {
+public interface NoteBookDao extends GenericDao <NoteBook, String> {
 }
